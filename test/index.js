1f9607475c7883c10fe07f0f0d205fba0e4b5818
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-11 14:46:28 +0200

    test: add serializer test

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 const test = require('ava')
 const createCompress = require('..')
+const v8 = require('v8')
 
 ;[
   ['null', null],
@@ -16,4 +17,15 @@ const createCompress = require('..')
     const decompressedData = await decompress(compressedData)
     t.deepEqual(decompressedData, value)
   })
+})
+
+test('custom serializer/deserializer', async t => {
+  const { compress, decompress } = createCompress({
+    serialize: v8.serialize,
+    deserialize: v8.deserialize
+  })
+  const value = { foo: 'bar', fooz: { foo: 'bar' }, arr: [1, 2, 3, 4] }
+  const compressedData = await compress(value)
+  const decompressedData = await decompress(compressedData)
+  t.deepEqual(decompressedData, value)
 })
