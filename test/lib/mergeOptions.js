b00033ce76e6ac318e55ba8c85db2f9e6d0dbb7d
Artem Kudla <kudla.artem@gmail.com>
2021-08-21 00:20:10 +0300

    test: tests for brotli options

---
+++ test/lib/mergeOptions.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const {
+  constants: {
+    BROTLI_PARAM_QUALITY,
+    BROTLI_MIN_QUALITY,
+    BROTLI_MODE_TEXT,
+    BROTLI_PARAM_MODE
+  }
+} = require('zlib')
+
+const test = require('ava')
+
+const { mergeOptions } = require('../../lib/mergeOptions')
+
+test('should skip empty inputs', t => {
+  t.deepEqual(mergeOptions(), {})
+})
+
+test('should skip first empty inputs', t => {
+  const options = { flush: 0 }
+  t.deepEqual(mergeOptions(undefined, options), options)
+})
+
+test('should skip second empty inputs', t => {
+  const options = { flush: 0 }
+  t.deepEqual(mergeOptions(options), options)
+})
+
+test('should override plain options', t => {
+  const options1 = { flush: 0, finishFlush: 1 }
+  const options2 = { chunkSize: 1024, finishFlush: 2 }
+  t.deepEqual(mergeOptions(options1, options2), { ...options1, ...options2 })
+})
+
+test('should deep merge params', t => {
+  const options1 = {
+    params: {
+      [BROTLI_PARAM_QUALITY]: 7,
+      [BROTLI_MIN_QUALITY]: 1
+    }
+  }
+  const options2 = {
+    params: {
+      [BROTLI_PARAM_MODE]: BROTLI_MODE_TEXT,
+      [BROTLI_MIN_QUALITY]: 5
+    }
+  }
+
+  t.deepEqual(mergeOptions(options1, options2), {
+    params: {
+      ...options1.params,
+      ...options2.params
+    }
+  })
+})
